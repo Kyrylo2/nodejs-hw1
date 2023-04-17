13ed5b9481050988388c86f707c8c48fad1d4fbc
Kyrylo Savchenko <savchenko.kyrylo@gmail.com>
2023-04-17 19:21:01 +0300

    hw1

--- contacts.js
+++ contacts.js
@@ -1,5 +1,53 @@
-const fs = require('fs');
+const { promises: fs } = require('fs');
 const path = require('path');
+const { nanoid } = require('nanoid');
 
-const contactsPath = path.resolve(__dirname, 'contacts.json');
-console.log(contactsPath);
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+async function readContacts() {
+  const buffer = await fs.readFile(contactsPath);
+  return JSON.parse(buffer);
+}
+
+async function writeContacts(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
+
+async function listContacts() {
+  return readContacts();
+}
+
+async function getContactById(contactId) {
+  const contacts = await readContacts();
+  return contacts.find((contact) => contact.id === contactId) || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await readContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const [result] = contacts.splice(index, 1);
+  await writeContacts(contacts);
+
+  return result;
+}
+
+async function addContact(data) {
+  console.log('Add contact func:', data);
+  const newContact = { id: nanoid(), ...data };
+  const contacts = await readContacts();
+  contacts.push(newContact);
+  await writeContacts(contacts);
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 console.log('Start project');
+const { program } = require('commander');
+const contacts = require('./contacts');
+
+program
+  .option('-a, --action <type>', 'choose action', 'list')
+  .option('-id, --id <type>', 'choose id')
+  .option('-n, --name <type>', 'choose name')
+  .option('-e, --email <type>', 'choose email')
+  .option('-p, --phone <type>', 'choose phone');
+
+program.parse();
+
+const argv = program.opts();
+console.log('argv:', argv);
+
+const invokeAction = async ({ action, name, email, phone }) => {
+  console.log(action);
+  switch (action) {
+    case 'add':
+      const newContact = contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+    case 'remove':
+      const removedContact = await contacts.removeContact(id);
+      return console.log(removedContact);
+    case 'get':
+      const contactById = await contacts.getContactById(id);
+      return console.log(contactById);
+    case 'list':
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+};
+
+invokeAction(argv);
